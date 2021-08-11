94cc2718608e4216fc04c6059de878b8c3f3f4f0
eximenz <terpigoreva1992@gmail.com>
2021-08-11 08:33:09 +0300

    added config

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin') включить на продакшн
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    // header: './components/header/header.js',
+    headersFooters: './UIkit/headersFooters/headersFooters.js'
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    // new HTMLWebpackPlugin({
+    //   filename: 'header.html',
+    //   template: './components/header/header.pug'
+    // chunks: ['header'] чанки это и есть точки входа. Если они 
+    // включены, то страница использует только этот js, а если чанка нет,
+    // то подключает все js из точек входа.
+    // }),
+    new HTMLWebpackPlugin({
+      filename: 'headersFooters.html',
+      template: './UIkit/headersFooters/headersFooters.pug',
+      chunks: ['headersFooters']
+    }),
+    // new MiniCssExtractPlugin({
+    //   filename: '[name].[contenthash].css'
+    // })     подключить после всей верстки, чтоб вынести в отдельный  минимизированный файл Сss 
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'], // на продакшн заменить style-loader на MiniCssExtractPlugin.loader
+      },
+      {
+        test: /.(png|jpg|svg|jpeg|gif|ico)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[hash][ext]'
+        },
+        exclude: path.resolve(__dirname, 'src/fonts')
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
+        type: 'asset/resource',
+
+        include: path.resolve(__dirname, 'src/fonts'),
+        generator: {
+          filename: 'fonts/[name].[hash][ext]'
+        }
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true
+        }
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    }
+  }
+}
