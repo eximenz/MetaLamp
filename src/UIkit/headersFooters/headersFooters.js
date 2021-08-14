b5c6080ffa025f9ed044f3ed1786d63d15661de5
eximenz <terpigoreva1992@gmail.com>
2021-08-14 23:04:43 +0300

    added footer to UIkit

--- src/UIkit/headersFooters/headersFooters.js
+++ src/UIkit/headersFooters/headersFooters.js
@@ -1,2 +1,6 @@
 import '../nullstyle.scss';
+import '../../fonts/fonts.scss';
 import '../../components/header/header.js';
+import '../../components/foo/bigfoo.js';
+import './headersFooters.scss';
+
