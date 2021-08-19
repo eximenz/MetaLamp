b6131ef938be71de71e244c1df5d2c97b9f7ef64
eximenz <terpigoreva1992@gmail.com>
2021-08-19 17:30:44 +0300

    added logo

--- src/UIkit/headersFooters/headersFooters.js
+++ src/UIkit/headersFooters/headersFooters.js
@@ -3,4 +3,7 @@ import '../../fonts/fonts.scss';
 import '../../components/header/header.js';
 import '../../components/foo/bigfoo.js';
 import './headersFooters.scss';
+import '@fortawesome/fontawesome-free/css/brands.css';
+
+
 
