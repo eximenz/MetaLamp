5a2d0cae5657ec000f9d60c2bbc8141bc1f75d73
eximenz <terpigoreva1992@gmail.com>
2021-08-20 16:41:56 +0300

    added second foo

--- src/UIkit/headersFooters/headersFooters.js
+++ src/UIkit/headersFooters/headersFooters.js
@@ -2,6 +2,7 @@ import '../nullstyle.scss';
 import '../../fonts/fonts.scss';
 import '../../components/header/header.js';
 import '../../components/foo/bigfoo.js';
+import '../../components/foo/foo768.js';
 import './headersFooters.scss';
 import '@fortawesome/fontawesome-free/css/brands.css';
 
