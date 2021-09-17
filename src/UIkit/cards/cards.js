b72bf3b261008d1b17e6dd3bfecb2e1fa8385f50
eximenz <terpigoreva1992@gmail.com>
2021-09-17 15:24:21 +0300

    added date-dropdown

--- src/UIkit/cards/cards.js
+++ src/UIkit/cards/cards.js
@@ -1,1 +1,4 @@
 import '../../components/roomSelection/roomSelection.js';
+import '../nullstyle.scss';
+import '../../fonts/fonts.scss';
+import './cards.scss';
--- src/components/roomSelection/roomSelection.js
+++ src/components/roomSelection/roomSelection.js
@@ -1,3 +1,58 @@
 import 'jquery';
 import '../../../node_modules/air-datepicker/dist/css/datepicker.min.css';
 import '../../../node_modules/air-datepicker/dist/js/datepicker.min.js';
+import './roomSelection.scss'
+
+$(function () {
+  $('#start').datepicker({
+    minDate: new Date(),
+    clearButton: true,
+    autoClose: true,
+    prevHtml: `<div class="arrow_prew"></div>`,
+    nextHtml: `<div class="arrow_next"></div>`,
+    classes: "dropdown-date",
+
+  });
+});
+
+$('#start').datepicker({
+  navTitles: {
+    days: `<div class="navTitles">MM yyyy</div>`,
+  }
+})
+
+$('#end').datepicker({
+  navTitles: {
+    days: `<div class="navTitles">MM yyyy</div>`,
+  }
+})
+
+$(function () {
+  $('#end').datepicker({
+    minDate: new Date(),
+    clearButton: true,
+    autoClose: true,
+    prevHtml: `<div class="arrow_prew"></div>`,
+    nextHtml: `<div class="arrow_next"></div>`,
+    classes: "dropdown-date",
+  });
+});
+
+var $start = $('#start'),
+  $end = $('#end');
+$start.datepicker({
+  onSelect: function (fd, date) {
+    $end.data('datepicker')
+      .update('minDate', date);
+    $end.focus();
+  }
+})
+$end.datepicker({
+  onSelect: function (fd, date) {
+    $start.data('datepicker')
+      .update('maxDate', date)
+  }
+})
+
+
+
