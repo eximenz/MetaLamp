d73a08edb30a3ffd2a44f434cc5da8be4d0c4571
eximenz <terpigoreva1992@gmail.com>
2021-09-29 10:54:25 +0300

    added air-datepicker

--- src/components/roomSelection/roomSelection.js
+++ src/components/roomSelection/roomSelection.js
@@ -4,55 +4,73 @@ import '../../../node_modules/air-datepicker/dist/js/datepicker.min.js';
 import './roomSelection.scss'
 
 $(function () {
-  $('#start').datepicker({
+  $('.dropdown-date__range-from').datepicker({
     minDate: new Date(),
     clearButton: true,
-    autoClose: true,
     prevHtml: `<div class="arrow_prew"></div>`,
     nextHtml: `<div class="arrow_next"></div>`,
     classes: "dropdown-date",
-
+    range: true,
+    multipleDatesSeparator: " - ",
   });
 });
 
-$('#start').datepicker({
+$('.dropdown-date__range-from').datepicker({
   navTitles: {
-    days: `<div class="navTitles">MM yyyy</div>`,
+    days: `<div class="datepicker--nav-title">MM yyyy</div>`,
   }
 })
 
-$('#end').datepicker({
-  navTitles: {
-    days: `<div class="navTitles">MM yyyy</div>`,
+
+
+$('.dropdown-date__range-from').datepicker({
+  onSelect: function (fd, d, picker) {
+    $(".dropdown-date__range-from").val(fd.split("-")[0]);
+    $(".dropdown-date__range-to").val(fd.split("-")[1]);
   }
-})
+});
 
 $(function () {
-  $('#end').datepicker({
-    minDate: new Date(),
-    clearButton: true,
-    autoClose: true,
-    prevHtml: `<div class="arrow_prew"></div>`,
-    nextHtml: `<div class="arrow_next"></div>`,
-    classes: "dropdown-date",
-  });
+  addApplyButton();
+
+  let $applyButton = $('[data-action="apply"]');
+  $applyButton.on('click', hideDatePicker);
+
+  let $returnDatePicker = $('.dropdown-date__range-from');
+  $returnDatePicker.on('click', showDatePicker);
+
 });
 
-var $start = $('#start'),
-  $end = $('#end');
-$start.datepicker({
-  onSelect: function (fd, date) {
-    $end.data('datepicker')
-      .update('minDate', date);
-    $end.focus();
+function addApplyButton() {
+  let $isPickerButtons = $('.datepickers-container').find('.datepicker--buttons');
+  if ($isPickerButtons) {
+    $isPickerButtons.append(
+      '<span class="datepicker--button datepicker--button_apply" data-action="apply">Применить</span>'
+    );
   }
-})
-$end.datepicker({
-  onSelect: function (fd, date) {
-    $start.data('datepicker')
-      .update('maxDate', date)
-  }
-})
+}
+
+function hideDatePicker() {
+  let $isDatePicker = $('.dropdown-date');
+  $isDatePicker.removeClass('active');
+}
+function showDatePicker() {
+  let $isDatePicker = $('.dropdown-date');
+  $isDatePicker.addClass('active');
+}
+
+
+$(function () {
+  let $lalala = $('.dropdown-date__range-to');
+  $lalala.on('click', lalalaDatePicker);
+
+});
+
+function lalalaDatePicker() {
+  let $isDatePicker = $('.dropdown-date__range-from');
+  $isDatePicker.focus();
+}
+
 
 
 
